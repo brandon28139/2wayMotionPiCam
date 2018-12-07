2cb29208bd819cbd679b20db6323aeb3ea2f5e75
Brandon Taylor <brandon28139@gmail.com>
2018-12-06 23:19:29 -0500

    New updates

--- main.py
+++ main.py
@@ -14,7 +14,14 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+GPIO.setup(3, GPIO.OUT) # Set pin 3 to be a motion detector
 
-
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH and DetType == "button":
+                print("Button was pushed!")
+    elif  GPIO.output(3,1) and DetType == "motion":
+                print("Motion Detected!")
+    elif (GPIO.output(3,1) or GPIO.input(10) == GPIO.HIGH) and DetType == "both":
+                print("button or motion detected!")
 
 
